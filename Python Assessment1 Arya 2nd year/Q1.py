8e91a456c06ac4f084d6a7fe2eea688dd479ae1d
Swastik <swastiksanger@gmail.com>
2024-05-07 18:45:00 +0530

    Question 1 Solved

---
+++ Python Assessment1 Arya 2nd year/Q1.py
@@ -0,0 +1,16 @@
+"""1. Write a program to display only those numbers from a list that satisfy the following conditions
+The number must be divisible by five If the number is greater than 150, then skip it and move to the next number If the number is greater than 500, then stop the loop
+
+Given"""
+
+numbers = [12, 75, 150, 180, 145, 525, 50]
+
+for x in numbers:
+    if(x>500):
+        break
+    elif(x>150):
+        continue
+    
+    elif(x%5==0):
+        print(x,"\n")
+        
